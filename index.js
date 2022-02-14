bedc1337960af8a6924af3eb8238bec30a18db11
unknown <khhan1990@hanmail.net>
2022-02-14 22:52:21 +0900

    2022-02-14

---
+++ index.js
@@ -0,0 +1,15 @@
+const express = require("express");
+const app = express();
+const port = 5000;
+
+const mongoose = require("mongoose");
+mongoose
+    .connect(
+        "mongodb+srv://khhan90:hankyu5134@shopping-mall.kmtbt.mongodb.net/myFirstDatabase?retryWrites=true&w=majority"
+    )
+    .then(() => console.log("MongoDB is Connected..."))
+    .catch((err) => console.log(err));
+
+app.get("/", (req, res) => res.send("Hello World"));
+
+app.listen(port, () => console.log(`Example app listening on port ${port}`));
